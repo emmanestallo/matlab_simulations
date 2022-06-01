a61d69e97ad58c74ddca6a07807511530122149d
Emmanuel Jesus Estallo <erestallo@up.edu.ph>
2022-06-02 01:49:46 +0800

    updated

--- 161/progExer_3/ECE161_PE03_estallo.m
+++ 161/progExer_3/ECE161_PE03_estallo.m
@@ -48,7 +48,7 @@ xlabel('radians per sample')
 %% Direct Approach 
 rootsOfUnity = [zeros(1000,1000)]; 
 
-tic()
+
 for a = 0:999 
     for b = 0:999
         rootsOfUnity(a+1,b+1) = exp(-1i*2*pi*(a*b)/1000); 
@@ -59,6 +59,7 @@ end
 n = 0:999; 
 x = (cos((1394/4000)*pi*n + pi/4) + sin((2418/4000)*pi*n + pi/18))';
 
+tic()
 X = abs(rootsOfUnity*x);  
 phase = angle(rootsOfUnity*x);
 toc()  
