13a686e80d9e82d5d4b319e4f25e481c709704fe
Emmanuel Jesus Estallo <erestallo@up.edu.ph>
2022-06-03 22:39:43 +0800

    used for quiz

--- 161/progExer4/freq_samp.m
+++ 161/progExer4/freq_samp.m
@@ -1,10 +1,11 @@
-Hr = [zeros(1,12), 0.7, ones(1,3)]';
-M = 31; 
+Hr = [zeros(1,4), 0.6, ones(1,3)]';
+M = 15; 
 alpha = (M-1)/2; 
-a_kn = zeros(16,alpha); 
-x = 0:30;
+a_kn = zeros(8,alpha); 
 
-for k = 0:15
+x = 0:14;
+
+for k = 0:7
     for n = 0:alpha
         a_kn(k+1,n+1) = 2*cos(((2*pi*k)/M)*(alpha-n));
     end 
@@ -12,8 +13,8 @@ end
 
 h = inv(a_kn)*Hr; 
 
-h(16) = 2*h(16); 
-h(17:31) = flip(h(1:15));
+h(8) = 2*h(8); 
+h(9:15) = flip(h(1:7));
 
 figure 
 stem(x,h)
